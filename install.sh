9aa7ead15b1fa479e54678719523a9dc235c91de
Lourenzo <lourenzoflores.pv@gmail.com>
2026-09-11 19:56:47 -0300

    feat: add cachyos repo setup to install script

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ echo "Concluído!"
 if [ -d "config/caelestia/utils" ]; then
     sudo cp -rf config/caelestia/utils/* /usr/lib/python3.14/site-packages/caelestia/utils/
 fi
+
+# --- Configurar Repositório CachyOS ---
+echo "⚙️ Configurando o repositório do CachyOS..."
+if ! grep -q "cachyos" /etc/pacman.conf; then
+    curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+    tar xvf cachyos-repo.tar.xz
+    cd cachyos-repo
+    sudo ./cachyos-repo.sh
+    cd ..
+    rm -rf cachyos-repo cachyos-repo.tar.xz
+fi
